9d2094a5d2e7ff72591ea8a245b8e34269e94a6a
cheachwood <jcfelicite@gmail.com>
2013-03-07 10:19:21 +0100

    Update PretEmpruntActivity.java

--- quila/src/fr/jcf/pret/activities/PretEmpruntActivity.java
+++ quila/src/fr/jcf/pret/activities/PretEmpruntActivity.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 Clint Cheachwood
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package fr.jcf.pret.activities;
 
 import java.io.FileNotFoundException;
@@ -30,21 +45,7 @@ import fr.jcf.pret.listeners.ContactButtonOnClickListener;
 import fr.jcf.pret.listeners.PretEmpruntEnregOnClickListener;
 import fr.jcf.pret.modeles.PretEmprunt;
 import fr.jcf.pret.utils.UtilPret;
-/**
- * Copyright 2013 Clint Cheachwood
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
+
 /**
  * Activity principale
  * 
